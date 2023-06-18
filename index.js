0b811608df3a8f290a509f649fba3c0eef724c5a
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-06-18 15:57:36 +0200

    save total to local Storage

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let totalEl = document.getElementById("total")
-let count = 0
-let total = 0
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let totalEl = document.getElementById("total");
+let count = 0;
+let total = JSON.parse(localStorage.getItem("total"));
+
+totalEl.textContent = "Total points today: " + total;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1;
+  countEl.textContent = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    total += count
-    totalEl.textContent = 'Total points today: ' + total
-    countEl.textContent = 0
-    count = 0
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  total += count;
+  totalEl.textContent = "Total points today: " + total;
+  countEl.textContent = 0;
+  count = 0;
+  localStorage.setItem("total", JSON.stringify(total));
 }
